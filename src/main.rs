64186095b392255b196b2452b04b0b8e74cb42e1
itome <itometeam@gmail.com>
2019-08-05 17:21:52 +0900

    Simple integer compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let arg: i32 = args[1].parse().unwrap();
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", arg);
+    println!("  ret")
 }
