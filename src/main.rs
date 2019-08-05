2fd371086e7e7a088a4be39c1b18c97609aaf464
Takeshi Tsukamoto <itometeam@gmail.com>
2019-08-05 23:49:19 +0900

    Add plus and minus

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,29 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let arg: i32 = args[1].parse().unwrap();
+    let arg: &str = &args[1];
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, {}", arg);
+    println!("  mov rax, 0");
+
+    let mut operator: char = '+';
+    let mut num: String = "".to_string();
+    let arg = format!("{}{}", arg, "e");
+    for char in arg.chars() {
+        if char == '+' || char == '-' || char == 'e' {
+            if operator == '+' {
+                println!("  add rax, {}", num.parse::<usize>().unwrap());
+            }
+            if operator == '-' {
+                println!("  sub rax, {}", num.parse::<usize>().unwrap());
+            }
+            operator = char;
+            num = "".to_string();
+        } else {
+            num += &char.to_string();
+        }
+    }
+
     println!("  ret")
 }
--- tmp.s
+++ tmp.s
@@ -1,5 +1,8 @@
 .intel_syntax noprefix
 .global main
 main:
-  mov rax, 42
+  mov rax, 0
+  add rax, 5
+  add rax, 20
+  sub rax, 4
   ret
